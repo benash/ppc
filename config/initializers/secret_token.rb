c84333ae64116e5c38d9359fec109bf6811283b3
Ben Nash <benash@umich.edu>
2013-03-03 00:07:46 -0500

    Fresh Vanilla Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppc::Application.config.secret_token = '268ea237b5bdecda84f9b138217243b0d264579f5487b8336a0cc7de23d773765b8ee4d26293c8dc8caebffbac5947776e8c0cac3c37e7dfb7bb3599a75ecda3'
