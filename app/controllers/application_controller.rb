49e1144b9f981ba94078fc5d406fee6aa958d58d
Ben Nash <benash@umich.edu>
2013-03-04 04:03:45 -0500

    Rev 0.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :company, :full_company
+
+  def company
+    'LiquidSteel'
+  end
+
+  def full_company
+    company + ' Incorporated'
+  end
+
 end
